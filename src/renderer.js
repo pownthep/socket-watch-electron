1851698bad3ae1d6f76a77125f1f4c46cd01c76b
Pownthep Laokhunthot <pownthepofficial@gmail.com>
2020-05-07 22:31:30 +0700

    implemented basic synchronous media control via socket.io

--- src/renderer.js
+++ src/renderer.js
@@ -1,10 +1,46 @@
 const PlayerController = require('media-player-controller');
 const { dialog } = require('electron').remote;
+const io = require('socket.io-client');
+const socket = io('http://localhost:3000');
 
 let urlInput = document.getElementById('url-input');
+let time = document.getElementById('time');
 
 var player = new PlayerController({
     app: 'vlc'
+});
+
+urlInput.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        load(event.target.value);
+    }
+    socket.emit('url input', event.target.value);
+});
+
+socket.on('url input', function(msg) {
+    urlInput.value = msg;
+    load(urlInput.value);
+});
+
+socket.on('vlc cmd', function(msg) {
+    switch (msg) {
+        case 'cyclePause':
+            player.cyclePause();
+            break;
+        case 'cycleAudio':
+            player.cycleAudio();
+            break;
+        case 'cycleSubs':
+            player.cycleSubs();
+            break;
+        case 'cycleFullscreen':
+            player.cycleFullscreen();
+            break;
+        default:
+            break;
+    }
 });
 
 function selectMedia() {
@@ -21,36 +57,37 @@ function selectMedia() {
     });
 }
 
-urlInput.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        load(event.target.value);
-    }
-});
-
 function load(mediaPath) {
     player.load(mediaPath);
 }
 
-function cyclePause() {
-    player.cyclePause();
+function play() {
+    player.play();
+    socket.emit('vlc cmd', 'play');
+}
+
+function pause() {
+    player.pause();
+    socket.emit('vlc cmd', 'pause');
 }
 
 function cycleAudio() {
     player.cycleAudio();
+    socket.emit('vlc cmd', 'cycleAudio');
 }
 
 function cycleSubs() {
     player.cycleSubs();
+    socket.emit('vlc cmd', 'cycleSubs');
 }
 
 function cycleFullscreen() {
     player.cycleFullscreen();
+    socket.emit('vlc cmd', 'cycleFullscreen');
 }
 
 /* Path to file or link. Can be changed anytime without creating new player objects */
-player.opts.media = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+player.opts.media = 'https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4';
 
 player.launch(err => {
     if (err) {
@@ -58,4 +95,10 @@ player.launch(err => {
     }
 });
 
-player.on('playback', console.log);
+
+function timer(data) {
+    if (data.name === 'time-pos') time.innerText = data.value;
+}
+player.on('playback', function(data) {
+    if (data.name === 'time-pos') time.innerText = data.value;
+});
